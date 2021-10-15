51cf773eadc5217d0ac469ec039cd3d890de0d5e
Georgi Petrov <georgipeetrov@gmail.com>
2021-10-15 20:23:51 +0300

    making a custom theme

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Notes from './pages/Notes'
 import Create from './pages/Create'
+import { ThemeProvider } from '@material-ui/core';
+import { createTheme } from '@material-ui/core/styles';
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#fefefe'
+      }
+    }
+  })
+
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Notes />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
